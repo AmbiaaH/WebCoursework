138b0eee43ba6379a20c94ac0ef2dd22fd19f5c6
AmbiaaH <ambiaahussain@gmail.com>
2024-12-30 12:41:43 +0200

    created required views for authentication and basic configs

--- api/views.py
+++ api/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse, HttpRequest, JsonResponse
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-
+#This Main page should be accessed after the user is logged in
+@login_required
 def main_spa(request: HttpRequest) -> HttpResponse:
     return render(request, 'api/spa/index.html', {})
+
+def login(request):
+    return render(request, 'api/spa/login.html',{})
+
+def register(request):
+    return render(request, 'api/spa/register.html',{})
