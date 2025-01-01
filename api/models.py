c060a9babcd48a48a009f9d35938ba810a47271d
AmbiaaH <ambiaahussain@gmail.com>
2025-01-02 01:18:19 +0200

    created db schema and custom user model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
@@ -8,3 +10,28 @@ class PageView(models.Model):
 
     def __str__(self):
         return f"Page view count: {self.count}"
+
+class Hobby(models.Model):
+    title = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.title
+    
+class User(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    hobbies = models.ManyToManyField(Hobby)
+    friends = models.ManyToManyField('self', symmetrical=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}".strip()
+
+class Status(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+class FriendRequest(models.Model):
+    sender_id = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_requests', on_delete=models.CASCADE)
+    receiver_id = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='received_requests', on_delete=models.CASCADE)
+    status_id = models.ForeignKey(Status, on_delete=models.SET_DEFAULT, default=1)
