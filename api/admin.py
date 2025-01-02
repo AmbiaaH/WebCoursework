5d5af7ab0cba03bdae7fd47a5fd574a3df0aebf7
AmbiaaH <ambiaahussain@gmail.com>
2025-01-02 02:02:00 +0200

    registered models for admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Hobby, Status, FriendRequest
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+admin.site.register(Hobby)
+admin.site.register(Status)
+admin.site.register(FriendRequest)
